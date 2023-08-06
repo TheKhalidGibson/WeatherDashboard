6258fb7c81c5f6bc48ad8f20313e2b279a6185a7
Khalid G <kgibson4795@gmail.com>
2023-08-05 23:53:47 -0400

    attempted to create the layout of dashboard with bootstrap

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=dd13cf1650c8484f942a9cd1a13f9ff5")
+// The following fetch should be nested inside of an event listener
+
+fetch("https://api.openweathermap.org/geo/1.0/direct?q=Dallas&limit=5&appid=dd13cf1650c8484f942a9cd1a13f9ff5")
     .then(response => response.json())
     .then(citiesFound => {
         let firstCity = citiesFound[0];
@@ -9,8 +11,10 @@ fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=dd13
 
     })
 
-
+    // This promise corresponds with the return fetch above.. once the return fetch is returned it will activate this promise
+    
     .then(response => response.json())
     .then(data => {
+
         console.log(data);
     })
