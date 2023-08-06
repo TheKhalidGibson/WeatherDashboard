62e42e2944d0fa8a51c3c4f81a1b1c42fa56f2e2
Khalid G <kgibson4795@gmail.com>
2023-08-06 15:59:44 -0400

    added a horizontal line to separate the search from the generated results section.. created a heading for the 5 day forecat section

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let searchEl = document.querySelector("#search")
 
 
 
@@ -5,25 +6,28 @@
 
 
 
+searchEl.addEventListener("click", function(){
 
 
-// The following fetch should be nested inside of an event listener
+    // The following fetch should be nested inside of an event listener
 
-fetch("https://api.openweathermap.org/geo/1.0/direct?q=Dallas&limit=5&appid=dd13cf1650c8484f942a9cd1a13f9ff5")
-    .then(response => response.json())
-    .then(citiesFound => {
-        let firstCity = citiesFound[0];
-        console.log(firstCity.lat);
-        console.log(firstCity.lon);
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q=Dallas&limit=5&appid=dd13cf1650c8484f942a9cd1a13f9ff5")
+        .then(response => response.json())
+        .then(citiesFound => {
+            let firstCity = citiesFound[0];
+            console.log(firstCity.lat);
+            console.log(firstCity.lon);
 
-        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=dd13cf1650c8484f942a9cd1a13f9ff5`)
+            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=dd13cf1650c8484f942a9cd1a13f9ff5`)
 
-    })
+        })
 
-    // This promise corresponds with the return fetch above.. once the return fetch is returned it will activate this promise
-    
-    .then(response => response.json())
-    .then(data => {
+        // This promise corresponds with the return fetch above.. once the return fetch is returned it will activate this promise
+        
+        .then(response => response.json())
+        .then(data => {
 
-        console.log(data);
-    })
+            console.log(data);
+        })
+
+});
