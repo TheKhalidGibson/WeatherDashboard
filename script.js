318e251488c8a6bb8ab52af21718c80bcf689840
Khalid G <kgibson4795@gmail.com>
2023-08-06 19:50:37 -0400

    worked with tutor and he showed me alternative ways to structure my javascript and get the data

--- script.js
+++ script.js
@@ -1,52 +1,112 @@
-let searchEl = document.querySelector("#search")
-let searchList = document.querySelector("#search-list")
+let searchEl = document.querySelector("#search");
+let searchList = document.querySelector("#search-list");
 let searchResults = [];
+const apiKey = "dd13cf1650c8484f942a9cd1a13f9ff5"
 
-function renderSearchResults(){
+const searchInput = document.querySelector("input")
+// function renderSearchResults(){
 
-    for (let i = 0; i < searchResults.length; i++) {
-     
-        let searchResults = searchResults[i];
-
-        let li = document.createElement("li");
-        li.textContent = searchResults;
-        li.setAttribute("data-index", i);
-
-
-
+    //     for (let i = 0; i < searchResults.length; i++) {
         
-    }
+        //         let searchResults = searchResults[i];
 
-}
-
-
-
-
+//         let li = document.createElement("li");
+//         li.textContent = searchResults;
+//         li.setAttribute("data-index", i);
 
 
+//         searchList.appendChild(li);
 
-searchEl.addEventListener("click", function(){
+
+//     }
+
+// }
 
 
-    // The following fetch should be nested inside of an event listener
+function fetchCoordinates(searchQuery = "") {
 
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q=Dallas&limit=5&appid=dd13cf1650c8484f942a9cd1a13f9ff5")
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${searchQuery}&limit=5&appid=${apiKey}`)
         .then(response => response.json())
         .then(citiesFound => {
             let firstCity = citiesFound[0];
             console.log(firstCity.lat);
             console.log(firstCity.lon);
 
-            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=dd13cf1650c8484f942a9cd1a13f9ff5`)
+            return firstCity
 
+        }).catch((err) => {
+            console.log(err)
         })
+}
 
-        // This promise corresponds with the return fetch above.. once the return fetch is returned it will activate this promise
-        
+// This promise corresponds with the return fetch above.. once the return fetch is returned it will activate this promise
+function fetchCurrentWeather(params = { lat: "0", lon: "0" }) {
+
+
+    fetch(`https://api.openweathermap.org/geo/1.0/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
         .then(response => response.json())
-        .then(data => {
+        .then(citiesFound => {
 
-            console.log(data);
+            console.log(citiesFound);
         })
+}
+
+
+// [i].value = localStorage.getItem("inputCity");
+
+searchEl.addEventListener("click", function (event) {
+
+    event.preventDefault()
+
+    
+    console.log(searchInput.value)
+    console.log(event.target)
+
+    const coordinates = fetchCoordinates(searchInput.value)
+    console.log(coordinates)
+
+    if (!coordinates.lat && !coordinates.lon) {
+        console.log("coordinates are falsy")
+        return null
+    }
+
+
+    const currentWeather = fetchCurrentWeather(coordinates)
+        console.log()
+    // function renderSearchResults(){
+
+    //     for (let i = 0; i < searchResults.length; i++) {
+
+    //         let searchResults = searchResults[i];
+
+    //         let li = document.createElement("li");
+    //         li.textContent = searchResults;
+    //         li.setAttribute("data-index", i);
+
+
+    //         searchList.appendChild(li);
+
+
+    //     }
+
+    // }
+
+    // if (event.target.matches("button")) {
+
+
+
+
+
+    //     localStorage.setItem()
+
+    // }
+
+
+
+
+    // The following fetch should be nested inside of an event listener
+
 
 });
+
+// searchList.addEventListener("click", function (event))
