73698bab70fba2ed257827da2aec446ef9f62b9a
Khalid G <kgibson4795@gmail.com>
2023-08-05 20:55:04 -0400

    replaced my api call example with a proper one with lat and lon values.. added bootstrap for simple form with search bar and editted it to resemble mock up.. linked bootstrap

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=dd13c
     })
 
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=dd13cf1650c8484f942a9cd1a13f9ff5")
+fetch("api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=dd13cf1650c8484f942a9cd1a13f9ff5")
     .then(response => response.json())
     .then(data => {
         console.log(data);
