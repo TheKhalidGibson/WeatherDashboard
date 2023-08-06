cb0fed9b26b09fd18a82773256bc206e58a91a70
Khalid G <kgibson4795@gmail.com>
2023-08-05 21:26:47 -0400

    string interpolated the return fetch and changed the number values of lat and long into j query values

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=dd13
         console.log(firstCity.lat);
         console.log(firstCity.lon);
 
-        return fetch('https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=dd13cf1650c8484f942a9cd1a13f9ff5')
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=dd13cf1650c8484f942a9cd1a13f9ff5`)
 
     })
 
